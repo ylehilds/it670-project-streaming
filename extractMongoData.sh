be7e8033a1ddd4caef404e7ca9936d4b3e4ab2da
ylehilds <lehi_alcantara@byu.edu>
2019-11-29 15:36:20 -0700

    added some instructions how to see data in mongo.

--- extractMongoData.sh
+++ extractMongoData.sh
@@ -1,1 +1,12 @@
+#instructions on how to see the data in mongo via terminal
+#conceptually:
+#mongo
+#use <desired_database_name>
+#db.<desired_collection>.find({}).pretty()
+
+#example:
+#mongo
+#use thanksgiving
+#db.stream.find({}).pretty()
+
 mongoexport --host localhost --db thanksgiving --collection stream --type=csv --out thanksgiving.csv --fields id,user,text
